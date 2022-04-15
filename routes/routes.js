3470c1895d1bc5f616de7f7d8c46a33f33841496
n0e <noemy.garcia@gmail.com>
2022-04-15 20:57:34 +0200

    [ADD] More pugs

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const routes = require('express').Router();
 const products = require('../controllers/product_controller');
 
 routes.get('/', (req, res) => {
-    res.send('Regulinchi!')
+    res.render('theme');
 });
 
 module.exports = routes;
